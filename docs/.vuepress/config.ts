75e575ad7edd4278271407fff282ae9caca9ab70
张 红科 <1432461572@qq.com>
2022-08-07 18:25:45 +0800

    111

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -200,7 +200,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     footer: {
       createYear: 2022, // 博客创建年份
       copyrightInfo:
-        'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签或换行标签</br>
+        'MarcsZhang |  <a id="aa" href="http://beian.miit.gov.cn">京ICP备2021035196号-1 </a>', // 博客版权信息，支持a标签或换行标签</br>
     },
 
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
